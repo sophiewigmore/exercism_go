f17829b8c2f0ee6cea74178da5bc7dec3f9e4bb4
Sophie Wigmore <wigmore.s@husky.neu.edu>
2019-01-22 17:58:57 -0500

    go tutorial 4

---
+++ go/raindrops/raindrops.go
@@ -0,0 +1,29 @@
+package raindrops
+
+import (
+	"strconv"
+)
+
+//Raindrops: converts a number to a string:
+/**
+* If the number has 3 as a factor, output 'Pling'.
+* If the number has 5 as a factor, output 'Plang'.
+* If the number has 7 as a factor, output 'Plong'.
+* If the number does not have 3, 5, or 7 as a factor, just pass the number's digits straight through.
+**/
+func Convert(x int) string {
+	converted := ""
+	if x%3 == 0 {
+		converted += "Pling"
+	}
+	if x%5 == 0 {
+		converted += "Plang"
+	}
+	if x%7 == 0 {
+		converted += "Plong"
+	}
+	if len(converted) == 0 {
+		converted = strconv.Itoa(x)
+	}
+	return converted
+}
