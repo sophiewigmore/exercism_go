95966d2b9aa1fc5e37f1123823be84b60df7e580
Sophie Wigmore <wigmore.s@husky.neu.edu>
2019-01-23 17:22:41 -0500

    gofmt changes for tutorial 4

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -4,13 +4,13 @@ import (
 	"strconv"
 )
 
-//Raindrops: converts a number to a string:
+//Convert : converts a number to a string:
 /**
 * If the number has 3 as a factor, output 'Pling'.
 * If the number has 5 as a factor, output 'Plang'.
 * If the number has 7 as a factor, output 'Plong'.
 * If the number does not have 3, 5, or 7 as a factor, just pass the number's digits straight through.
-**/
+ */
 func Convert(x int) string {
 	converted := ""
 	if x%3 == 0 {
