09d1084c63fd28d6da0ee253f40259fdb9cb134c
Sophie Wigmore <wigmore.s@husky.neu.edu>
2019-01-29 12:12:01 -0500

    triangle - tutorial 5

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -1,28 +1,44 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package triangle should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package triangle
 
+import "math"
 
-// Notice KindFromSides() returns this type. Pick a suitable data type.
-type Kind
+// Kind stores the type of triangle.
+type Kind int
 
 const (
-    // Pick values for the following identifiers used by the test program.
-    NaT // not a triangle
-    Equ // equilateral
-    Iso // isosceles
-    Sca // scalene
+	// NaT is not a triangle
+	NaT Kind = 0
+	// Equ = equilateral
+	Equ Kind = 1
+	// Iso = isosceles
+	Iso Kind = 2
+	// Sca = scalene
+	Sca Kind = 3
 )
 
-// KindFromSides should have a comment documenting it.
+// KindFromSides returns the type of triangle based off of the sides
 func KindFromSides(a, b, c float64) Kind {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	var k Kind
-	return k
+	if SidesAreBadArgs(a, b, c) || RuleOfInequality(a, b, c) {
+		return NaT
+	}
+	if a == b && a == c {
+		return Equ
+	}
+	if a == b || a == c || b == c {
+		return Iso
+	}
+
+	return Sca
+}
+
+// SidesAreBadArgs is a method that returns if any of the sides are of length 0 or less
+func SidesAreBadArgs(a, b, c float64) bool {
+	return a <= 0 || b <= 0 || c <= 0 || math.IsNaN(a+b+c) || math.IsInf(a+b+c, 1)
+}
+
+// RuleOfInequality is a method that returns if the sides of a triangle disobey triangle equality rules.
+// the sum of the lengths of any two sides must be
+// greater than or equal to the length of the remaining side.
+func RuleOfInequality(a, b, c float64) bool {
+	return a+b < c || a+c < b || b+c < a
 }
