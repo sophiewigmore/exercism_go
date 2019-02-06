5c76365e5b31af434a00c023f774897181bb7a8c
Sophie Wigmore <wigmore.s@husky.neu.edu>
2019-02-06 17:30:55 -0500

    edits to scrabble tutorial to make it more readable

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,45 +1,33 @@
 package scrabble
 
-import "strings"
+import "unicode"
+
+// pointConversion is the map for letter -> points conversion
+var pointConversion = GetPointsMap()
 
 // Score is a method that computes the scrabble score of a word
 func Score(words string) int {
-	pointConversion := GetPointsMap()
 	points := 0
-	for i := 0; i < len(words); i++ {
-		points += pointConversion[strings.ToUpper(words[i:i+1])]
+	// turn the string into runes
+	wordAsRunes := []rune{}
+	for _, r := range words {
+		wordAsRunes = append(wordAsRunes, r)
+	}
+	for i := 0; i < len(wordAsRunes); i++ {
+		points += pointConversion[unicode.ToUpper(wordAsRunes[i])]
 	}
 	return points
 }
 
 //GetPointsMap returns the mapping of Letter to Point conversion
-func GetPointsMap() map[string]int {
-	pointConversion := make(map[string]int)
-	pointConversion["A"] = 1
-	pointConversion["E"] = 1
-	pointConversion["I"] = 1
-	pointConversion["O"] = 1
-	pointConversion["U"] = 1
-	pointConversion["L"] = 1
-	pointConversion["N"] = 1
-	pointConversion["R"] = 1
-	pointConversion["S"] = 1
-	pointConversion["T"] = 1
-	pointConversion["D"] = 2
-	pointConversion["G"] = 2
-	pointConversion["B"] = 3
-	pointConversion["C"] = 3
-	pointConversion["M"] = 3
-	pointConversion["P"] = 3
-	pointConversion["F"] = 4
-	pointConversion["H"] = 4
-	pointConversion["V"] = 4
-	pointConversion["W"] = 4
-	pointConversion["Y"] = 4
-	pointConversion["K"] = 5
-	pointConversion["J"] = 8
-	pointConversion["X"] = 8
-	pointConversion["Q"] = 10
-	pointConversion["Z"] = 10
+func GetPointsMap() map[rune]int {
+	pointConversion := map[rune]int{
+		'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+		'D': 2, 'G': 2,
+		'B': 3, 'C': 3, 'M': 3, 'P': 3,
+		'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+		'K': 5,
+		'J': 8, 'X': 8,
+		'Q': 10, 'Z': 10}
 	return pointConversion
 }
