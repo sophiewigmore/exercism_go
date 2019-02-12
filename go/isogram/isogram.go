0caf5391d1cf4f1265f5d65a7164d672f16f071c
Sophie Wigmore <wigmore.s@husky.neu.edu>
2019-02-12 17:48:47 -0500

    fix isogram code to be cleaner

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,24 +1,25 @@
 package isogram
 
-import "strings"
+import "unicode"
 
 // IsIsogram returns if a string is an isogram
 func IsIsogram(input string) bool {
 	if len(input) == 0 {
 		return true
 	}
-	m := make(map[rune]int)
-	letters := []rune(strings.ToLower(input))
+	m := make(map[rune]bool)
+	letters := []rune(input)
 
 	for _, letter := range letters {
-		if letter != '-' && letter != ' ' {
-			_, ok := m[letter]
-			if !ok {
-				m[letter] = 0
-			} else {
-				return false
-			}
+		letter = unicode.ToLower(letter)
+		if letter == ' ' || letter == '-' {
+			continue
 		}
+		if m[letter] {
+			return false
+		}
+		m[letter] = true
+
 	}
 	return true
 }
