2347817015edaf293747cd758c20888107a3aca6
Sophie Wigmore <wigmore.s@husky.neu.edu>
2019-02-19 16:59:59 -0500

    Go tutorial for sum of squares and square of sums

---
+++ go/difference-of-squares/difference_of_squares.go
@@ -0,0 +1,21 @@
+package diffsquares
+
+// SquareOfSum: Returns the square of the sums of the natural numbers up to N
+func SquareOfSum(n int) int {
+	// formula for sum of first N natual nums: (number of terms * (first term+last term))/2
+	sum:=(n*(n+1))/2
+	return sum * sum
+}
+
+
+// SumOfSquares: Returns sum of the square of natural numbers up to N
+func SumOfSquares(n int) int {
+	// formula for the sum of squares
+	return (n * (n+1) * ((2*n)+1))/6
+}
+
+
+// Difference:  Find the difference between the square of the sum and the sum of the squares of the first N natural numbers.
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
